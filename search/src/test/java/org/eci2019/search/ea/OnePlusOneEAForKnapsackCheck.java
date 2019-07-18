856bb54d010b9d35d563cb204969be465fa1bbc4
arcuri82 <arcuri82@gmail.com>
2019-07-18 16:22:57 +0200

    working on les04

---
+++ search/src/test/java/org/eci2019/search/ea/OnePlusOneEAForKnapsackCheck.java
@@ -0,0 +1,43 @@
+package org.eci2019.search.ea;
+
+import org.eci2019.search.knapsack.GreedyForKnapsack;
+import org.eci2019.search.knapsack.KnapsackInstanceWithSolution;
+
+public class OnePlusOneEAForKnapsackCheck {
+
+
+    private static void check(KnapsackInstanceWithSolution p, int iterations) {
+
+        boolean[] greedy = GreedyForKnapsack.solveByBestRatioFirst(p.getProblem());
+
+        boolean[] opoea = OnePlusOneEAForKnapsack.solve(iterations, p.getProblem());
+
+        double fg = p.getProblem().evaluate(greedy);
+        double og = p.getProblem().evaluate(opoea);
+
+        System.out.print("N=" + p.getProblem().getSize());
+        System.out.print(" , Greedy=" + fg);
+        System.out.print(" , (1+1)EA=" + og);
+
+        String label;
+        if (og == p.getBestFitness()) {
+            label = "Optimal";
+        } else if (og > fg) {
+            label = "Improved";
+        } else {
+            label = "Worse";
+        }
+
+        System.out.println("  ->  " + label);
+    }
+
+    public static void main(String[] args) {
+
+        int iterations = 100_000;
+
+        check(KnapsackInstanceWithSolution.problemP05(), iterations);
+        check(KnapsackInstanceWithSolution.problemP15(), iterations);
+        check(KnapsackInstanceWithSolution.problemP24(), iterations);
+        check(KnapsackInstanceWithSolution.problemP100(), iterations);
+    }
+}
